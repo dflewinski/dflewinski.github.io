80cd41346453dcf7dc46d1fa45d3e4f260931890
DFL <38264143+dflewinski@users.noreply.github.com>
2021-01-15 04:14:41 +0100

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,11 +1,30 @@
 //wczytywanie danych
-var map = L.map('mapid').setView([54.186, 18.031], 8);
+var map = L.map('mapid').setView([54.186, 18.031],7);
 
-map.options.minZoom = 8;
+map.options.minZoom = 9;
 map.options.maxZoom = 11;
 
-//todo - replace hardcoded colors by table
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Koszty lekcji religii w gminach i miastach województwa pomorskiego</h4>' +
+        '<p>Dane zebrane przez użytkowników grupy <a href="https://www.facebook.com/groups/1584850021763935/">Świecka szkoła Pomorze</a> za pomocą wniosków do lokalnych samorządów o udzielenie informacji publicznej.</p>'
+    this._div.innerHTML +=
+        '<div>Źródła:</div>'
+        + '<div><a href="https://dane.gov.pl/pl/dataset/288,dane-jednostkowe-przedszkoli-szko-i-placowek-oswiatowych-w-latach-2012-2018/resource/26041/table">Liczba uczniów w szkołach.</a></div>'
+};
+
+info.addTo(map);
+
+//replace hardcoded colors by table
 var LEGENDPALETTE8 = [
+    '#53000b',
     '#99000d',
     '#cb181d',
     '#ef3b2c',
@@ -19,7 +38,9 @@ var LEGENDPALETTE8 = [
 var STATUS = L.geoJson(gminyData, {style: styleStatus, onEachFeature: STATUSonEachFeature}).addTo(map);
 var PERSTUDENT = L.geoJson(gminyData, {style: stylePerStudent, onEachFeature: PERSTUDENTonEachFeature});
 var COST = L.geoJson(gminyData, {style: styleCost, onEachFeature: COSTonEachFeature});
-var PERCITIZEN = L.geoJson(gminyData, {style: stylePerCitizen, onEachFeature: PERCITIZENonEachFeature}); //todo
+var PERCITIZEN = L.geoJson(gminyData, {style: stylePerCitizen, onEachFeature: PERCITIZENonEachFeature});
+var GMINACOST = L.geoJson(gminyData, {style: styleGminaCost, onEachFeature: GMINACOSTonEachFeature});
+var SUBVENTION = L.geoJson(gminyData, {style: styleSubvention, onEachFeature: SUBVENTIONonEachFeature});
 
 //kolory do skal
 function getColorByCost(d) {
@@ -49,11 +70,11 @@ function getColorByPerCitizen(d) {
     return d > 70 ? LEGENDPALETTE8[0] :
         d > 60 ? LEGENDPALETTE8[1] :
             d > 50 ? LEGENDPALETTE8[2] :
-                d > 40 ? LEGENDPALETTE8[3]:
+                d > 40 ? LEGENDPALETTE8[3] :
                     d > 30 ? LEGENDPALETTE8[4] :
                         d > 20 ? LEGENDPALETTE8[5] :
                             d > 10 ? LEGENDPALETTE8[6] :
-                                d > 1 ? LEGENDPALETTE8[7] :
+                                d >= 1 ? LEGENDPALETTE8[7] :
                                     LEGENDPALETTE8[8];
 }
 
@@ -66,17 +87,52 @@ function stylePerCitizen(feature) {
         fillOpacity: 0.7,
         fillColor: getColorByPerCitizen(feature.properties.perCitizen)
     };
-    return;
 }
 
 function getColorByGminaCost(d) {
-    //todo
-    return;
+    return d > 120000 ? LEGENDPALETTE8[0] :
+        d > 100000 ? LEGENDPALETTE8[1] :
+            d > 80000 ? LEGENDPALETTE8[2] :
+                d > 60000 ? LEGENDPALETTE8[3] :
+                    d > 40000 ? LEGENDPALETTE8[4] :
+                        d > 20000 ? LEGENDPALETTE8[5] :
+                            d > 0 ? LEGENDPALETTE8[6] :
+                                d === ' ' ? LEGENDPALETTE8[8] :
+                                    '#3cc31f'; //vhen value is 0
 }
 
 function styleGminaCost(feature) {
-    //todo
-    return;
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7,
+        fillColor: getColorByGminaCost(feature.properties.gminaCost)
+    };
+}
+
+function getColorBySubvention(d) {
+    return d > 1200000 ? LEGENDPALETTE8[0] :
+        d > 1000000 ? LEGENDPALETTE8[1] :
+            d > 800000 ? LEGENDPALETTE8[2] :
+                d > 600000 ? LEGENDPALETTE8[3] :
+                    d > 400000 ? LEGENDPALETTE8[4] :
+                        d > 200000 ? LEGENDPALETTE8[5] :
+                            d > 0 ? LEGENDPALETTE8[6] :
+                                d === ' ' ? LEGENDPALETTE8[8] :
+                                    '#3cc31f'; //when value is 0
+}
+
+function styleSubvention(feature) {
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7,
+        fillColor: getColorBySubvention(feature.properties.subvention)
+    };
 }
 
 function getColorByStatus(d) {
@@ -98,15 +154,15 @@ function styleStatus(feature) {
 }
 
 function getColorByPerStudent(d) {
-    return d > 800 ? '#99000d' :
-        d > 700 ? '#cb181d' :
-            d > 600 ? '#ef3b2c' :
-                d > 500 ? '#fb6a4a' :
-                    d > 400 ? '#fc9272' :
-                        d > 300 ? '#fcbba1' :
-                            d > 200 ? '#fee0d2' :
-                                d > 50 ? '#fff5f0' :
-                                    '#151212';
+    return d > 800 ? LEGENDPALETTE8[0] :
+        d > 700 ? LEGENDPALETTE8[1] :
+            d > 600 ? LEGENDPALETTE8[2] :
+                d > 500 ? LEGENDPALETTE8[3] :
+                    d > 400 ? LEGENDPALETTE8[4] :
+                        d > 300 ? LEGENDPALETTE8[5] :
+                            d > 200 ? LEGENDPALETTE8[6] :
+                                d > 50 ? LEGENDPALETTE8[7] :
+                                    LEGENDPALETTE8[8];
 }
 
 function stylePerStudent(feature) {
@@ -225,32 +281,101 @@ PERCITIZENlegend.onAdd = function (map) {
             labels[i] + '<br>';
     }
     return div;
-    return;
+};
+
+var GMINACOSTlegend = L.control({position: 'bottomright'});
+GMINACOSTlegend.onAdd = function (map) { //fixme
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [
+            120001,
+            100001,
+            80001,
+            60001,
+            40001,
+            20001,
+            1,
+            0,
+            " "],
+        labels = [
+            '> 120 000 zł',
+            '120 000 zł - 100 000 zł',
+            '100 000 zł - 80 000 zł',
+            '80 000 zł - 60 000 zł',
+            '60 000 zł - 40 000 zł',
+            '40 000 zł - 20 000 zł',
+            '20 000 zł - 0 zł',
+            '0 zł',
+            'Brak danych'];
+
+    div.innerHTML += '<div><b>Roczne wydatki z budżetu gminy</b></div><br>'
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColorByGminaCost(grades[i]) + '"></i> ' +
+            labels[i] + '<br>';
+    }
+    return div;
+};
+
+var SUBVENTIONlegend = L.control({position: 'bottomright'});
+SUBVENTIONlegend.onAdd = function (map) { //fixme
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [
+            1200001,
+            1000001,
+            800001,
+            600001,
+            400001,
+            200001,
+            1,
+            0,
+            " "],
+        labels = [
+            '> 1 200 000 zł',
+            '1 200 000 zł - 1 000 000 zł',
+            '1 000 000 zł - 800 000 zł',
+            '800 000 zł - 600 000 zł',
+            '600 000 zł - 400 000 zł',
+            '400 000 zł - 200 000 zł',
+            '200 000 zł - 0 zł',
+            '0 zł',
+            'Brak danych'];
+
+    div.innerHTML += '<div><b>Roczna subwencja na nauczanie religii</b></div><br>'
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColorBySubvention(grades[i]) + '"></i> ' +
+            labels[i] + '<br>';
+    }
+    return div;
 };
 
 //wypór legendy
 var baseMaps = {
-    "Status zapyań": STATUS,
+    "Status zapytań": STATUS,
     "Koszt bezwzględny": COST,
     "Koszt na ucznia": PERSTUDENT,
-    "Koszt na mieszkańca": PERCITIZEN
-
+    "Koszt na mieszkańca": PERCITIZEN,
+    "Koszt gminy" : GMINACOST,
+    "Wysokość subwencji" : SUBVENTION
 };
 
 var overlayMaps = {};
 
 L.control.layers(baseMaps, overlayMaps, {
     collapsed: false,
-    position: 'bottomleft'
+    position: 'topright'
 }).addTo(map);
-
 
 STATUSlegend.addTo(map);
 currentLegend = STATUSlegend;
 
 //zmiana legendy przy zminie danych
 map.on('baselayerchange', function (eventLayer) {
-    if (eventLayer.name === 'Status zapyań') {
+    if (eventLayer.name === 'Status zapytań') {
         map.removeControl(currentLegend);
         currentLegend = STATUSlegend;
         STATUSlegend.addTo(map);
@@ -266,6 +391,14 @@ map.on('baselayerchange', function (eventLayer) {
         map.removeControl(currentLegend);
         currentLegend = PERCITIZENlegend;
         PERCITIZENlegend.addTo(map);
+    } else if (eventLayer.name === 'Koszt gminy') {
+        map.removeControl(currentLegend);
+        currentLegend = GMINACOSTlegend;
+        GMINACOSTlegend.addTo(map);
+    }else if (eventLayer.name === 'Wysokość subwencji') {
+        map.removeControl(currentLegend);
+        currentLegend = SUBVENTIONlegend;
+        SUBVENTIONlegend.addTo(map);
     }
 })
 
@@ -306,11 +439,15 @@ function getDescriptionPopup(feature) {
     content += getDescription(feature);
 
     if (feature.properties.subvention) {
-        content += '<div>Kwota subwencji oświatowej: ' + parseFloat(feature.properties.subvention).toLocaleString(undefined) + ' zł</div>';
+        if (feature.properties.subvention !== " ") {
+            content += '<div>Kwota subwencji oświatowej: ' + parseFloat(feature.properties.subvention).toLocaleString(undefined) + ' zł</div>';
+        }
     }
 
     if (feature.properties.gminaCost) {
-        content += '<div>Kwota z samorządu: ' + parseFloat(feature.properties.gminaCost).toLocaleString(undefined) + ' zł</div>';
+        if (feature.properties.gminaCost !== " ") {
+            content += '<div>Kwota z samorządu: ' + parseFloat(feature.properties.gminaCost).toLocaleString(undefined) + ' zł</div>';
+        }
     }
 
     if (feature.properties.link.length > 0) {
@@ -323,6 +460,7 @@ function getDescriptionPopup(feature) {
     return content;
 }
 
+//zachowanie tooltipa
 function STATUSonEachFeature(feature, layer) {
     layer.bindPopup(getDescriptionPopup(feature));
     layer.bindTooltip(getDescription(feature));
@@ -439,7 +577,7 @@ function COSTonEachFeature(feature, layer) {
     });
 }
 
-function PERCITIZENonEachFeature(feature, layer) {
+function GMINACOSTonEachFeature(feature, layer) {
     layer.bindPopup(getDescriptionPopup(feature));
     layer.bindTooltip(getDescription(feature));
 
@@ -459,7 +597,7 @@ function PERCITIZENonEachFeature(feature, layer) {
     });
 
     layer.on('mouseout', function (f, l) {
-        PERCITIZEN.resetStyle(layer);
+        GMINACOST.resetStyle(layer);
     });
 
     layer.on('click', function (f, l) {
@@ -468,32 +606,40 @@ function PERCITIZENonEachFeature(feature, layer) {
     });
 }
 
-// geojson = L.geoJson(gminyData, {
-//   style: style,
-//   onEachFeature: onEachFeature
-// }).addTo(map);
+function SUBVENTIONonEachFeature(feature, layer) {
+    layer.bindPopup(getDescriptionPopup(feature));
+    layer.bindTooltip(getDescription(feature));
+
+    layer.on('mouseover', function (f, l) {
+        layer.setStyle({
+            weight: 5,
+            color: '#666',
+            dashArray: '',
+            fillOpacity: 0.7
+        });
+        if (layer.isPopupOpen()) {
+            layer.closeTooltip();
+        }
+        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+            layer.bringToFront();
+        }
+    });
+
+    layer.on('mouseout', function (f, l) {
+        SUBVENTION.resetStyle(layer);
+    });
+
+    layer.on('click', function (f, l) {
+        map.fitBounds(layer.getBounds());
+        layer.closeTooltip();
+    });
+}
 
 L.tileLayer('', {
     attribution: '<a href="https://www.facebook.com/groups/1584850021763935/">Świecka szkoła Pomorze</a>. <a href="">Wykonał DFL</a> ',
 }).addTo(map);
 
-var info = L.control();
-
-info.onAdd = function (map) {
-    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-    this.update();
-    return this._div;
-};
-
-info.update = function (props) {
-    this._div.innerHTML = '<h4>Koszty lekcji religii w jednostkach samorządowych województwa pomorskiego</h4>' +
-        '<p>Dane zebrane przez użytkowników grupy <a href="https://www.facebook.com/groups/1584850021763935/">Świecka szkoła Pomorze</a> za pomocą wniosków do lokalnych samorządów o udzielenie informacji publicznej.</p>'
-    this._div.innerHTML +=
-        '<div>Źródła:</div>'
-        + '<div><a href="https://dane.gov.pl/pl/dataset/288,dane-jednostkowe-przedszkoli-szko-i-placowek-oswiatowych-w-latach-2012-2018/resource/26041/table">Liczba uczniów w szkołach.</a></div>'
-};
-
-info.addTo(map);
 
 
-
+//resize the geojson view to bounds
+map.fitBounds(STATUS.getBounds());
