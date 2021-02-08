e8507d670f6b50eb47bf52c6160fb461c70604e9
DFL <daw.lewinski@gmail.com>
2021-02-08 17:51:10 +0100

    change min zoom level

--- cena-religii/swietokrzyskie/js/map.js
+++ cena-religii/swietokrzyskie/js/map.js
@@ -1,7 +1,7 @@
 //wczytywanie danych
 var map = L.map('mapid').setView([54.186, 18.031], 7);
 
-map.options.minZoom = 4;
+map.options.minZoom = 7;
 map.options.maxZoom = 10.5;
 map.options.zoomSnap = 0.30;
 map.options.zoomDelta = 0.30;
