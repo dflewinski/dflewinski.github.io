d181844054a2725e24eabe202ddaafa48bfb7b43
DFL <daw.lewinski@gmail.com>
2021-02-10 04:12:47 +0100

    lubuskie

--- cena-religii/lubuskie/js/variables.js
+++ cena-religii/lubuskie/js/variables.js
@@ -52,11 +52,11 @@ var LEGENDPALETTE8 = [
 var infoText = {
     title: "Koszty lekcji religii w gminach i miastach województwa lubuskiego",
     content:"Obejmuje głównie szkoły podstawowe i przedszkola. Nie zawiera kosztów ponoszonych przez powiaty (np. większość szkół średnich), jeżeli nie wspomniano inaczej.",
-    dataSource:'Dane zebrane przez użytkowników grupy <a href="">Świecka szkoła lubuskie</a> przy użyciu wniosków do lokalnych samorządów o udzielenie informacji publicznej. Jeżeli posiadasz takie i chcesz nimi się podzielić, skontaktuj się z twórcą mapy.'
+    dataSource:'Dane zebrane przez użytkowników grupy <a href="https://www.facebook.com/groups/411081973522848">Świecka szkoła w Lubuskiem</a> przy użyciu wniosków do lokalnych samorządów o udzielenie informacji publicznej. Jeżeli posiadasz takie i chcesz nimi się podzielić, skontaktuj się z twórcą mapy.'
 }
 
 var footerText = {
-    datasource:'<a href="">Dane - Świecka szkoła lubuskie</a>',
+    datasource:'<a href="https://www.facebook.com/groups/411081973522848">Dane - Świecka szkoła w Lubuskiem</a>',
     separator:' | ',
     creator:'<a href="https://www.facebook.com/profile.php?id=100000663068342">Wizualizacja - DFL</a>'
 }
