db9757bd4c07dec9ad0c76748e21746a4091c491
DFL <daw.lewinski@gmail.com>
2021-04-22 14:26:27 +0200

    Fix: - cities in ow41

--- cena-religii/ow41/js/cities.js
+++ cena-religii/ow41/js/cities.js
@@ -4,234 +4,120 @@ var cities = {
         {
             "type": "Feature",
             "properties": {
-                "name": "Gdańsk"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    18.65478515625,
-                    54.34214886448341
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "name": "Bydgoszcz"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    18.0010986328125,
-                    53.1237017988457
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
                 "name": "Szczecin"
             },
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    14.556884765625002,
-                    53.38332836757156
+                    14.5843505859375,
+                    53.43899149184269
                 ]
             }
         },
         {
             "type": "Feature",
             "properties": {
-                "name": "Łódź"
+                "name": "Gryfino"
             },
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    19.4512939453125,
-                    51.77123920059797
+                    14.4854736328125,
+                    53.25042550620077
                 ]
             }
         },
         {
             "type": "Feature",
             "properties": {
-                "name": "Poznań"
+                "name": "Myślibórz"
             },
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    16.9354248046875,
-                    52.40577019043
+                    14.8480224609375,
+                    52.94201777829491
                 ]
             }
         },
         {
             "type": "Feature",
             "properties": {
-                "name": "Wrocław"
+                "name": "Świnoujście"
             },
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    17.0343017578125,
-                    51.103521942404186
+                    14.26300048828125,
+                    53.902720016840476
                 ]
             }
         },
         {
             "type": "Feature",
             "properties": {
-                "name": "Zielona Góra"
+                "name": "Gryfice"
             },
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    15.496215820312498,
-                    51.94087861255871
+                    15.1885986328125,
+                    53.90110181472825
                 ]
             }
         },
         {
             "type": "Feature",
             "properties": {
-                "name": "Gorzów Wielkopolski"
+                "marker-color": "#7e7e7e",
+                "marker-size": "medium",
+                "marker-symbol": "",
+                "name": "Nowogard"
             },
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    15.238037109375,
-                    52.72631243810711
+                    15.150146484375,
+                    53.67068019347264
                 ]
             }
         },
         {
             "type": "Feature",
             "properties": {
-                "name": "Katowice"
+                "name": "Łobez"
             },
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    19.017333984375,
-                    50.26476541187554
+                    15.6884765625,
+                    53.63812471860769
                 ]
             }
         },
         {
             "type": "Feature",
             "properties": {
-                "name": "Kraków"
+                "name": "Stargard"
             },
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    19.9346923828125,
-                    50.06066538593667
+                    15.0347900390625,
+                    53.31774904749089
                 ]
             }
         },
         {
             "type": "Feature",
             "properties": {
-                "name": "Warszawa"
+                "name": "Kamień Pomorski"
             },
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    21.0113525390625,
-                    52.22779941887071
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "name": "Toruń"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    18.621826171875,
-                    53.00817326643286
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "name": "Olsztyn"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    20.489501953125,
-                    53.78118084719588
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "name": "Białystok"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    23.1591796875,
-                    53.1335898292448
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "name": "Lublin"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    22.56591796875,
-                    51.244724592848236
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "name": "Rzeszów"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    22.005615234375,
-                    50.035973672195496
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "name": "Kielce"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    20.63232421875,
-                    50.86837814203458
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "name": "Opole"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    17.9132080078125,
-                    50.66339047047327
+                    14.75189208984375,
+                    53.97708957753223
                 ]
             }
         }
